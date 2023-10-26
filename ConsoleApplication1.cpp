6ad8362617b0b9bfc9f71d2d3f5942c54f83f5d0
Hrizzal <tim.zaz2006@mail.ru>
2023-10-26 10:56:48 +0300

    1

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int n;
+	int a = 0;
+	cout << "Введите число \n";
+	cin >> n;
+ 	for (int i = 0; i < n; i++)
+	{
+		a += i;
+	}
+	cout << "Сумма чисел: \n" << a;
+}
+
