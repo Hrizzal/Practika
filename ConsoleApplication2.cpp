f1c27121a28e274c11f231fabf2b1bb7cf865c53
Hrizzal <tim.zaz2006@mail.ru>
2023-10-26 14:31:05 +0300

    .

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -8,21 +8,26 @@ int main()
     int num[7];
     for (int i = 0; i < 7; i++)
     {
-        cin >> num[i];
-
+        cin >> num[i]
     }
     int max = num[0];
     int min = num[0];
+    int max_count;
+    int min_count;
         for (int i = 0; i < 7; i++)
         {
             if (max < num[i])
             {
                 max = num[i];
             }
-            if (min > num[i])
+            else if (max = num[i])
+            {max_count++}
+
+             if (min > num[i])
             {
                 min = num[i];
             }
+             else if(min = num[i])
             if (i == 7) { bool out = true;}
         }
         if (bool out = true) 
@@ -30,7 +35,7 @@ int main()
             cout << "Максимальное значение: " << max << endl;
             cout << "Минимальное значение: " << min << endl;
         }
-        return 0;
+
 }
        
     
---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int num[7];
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> num[i];
+    }
+    int max = num[0];
+    int min = num[0];
+    int max_count = 0;
+    int min_count = 0;
+    for (int i = 0; i < 7; i++)
+    {
+        if (max < num[i]){max = num[i];}
+        if (min > num[i]){min = num[i];}
+    }
+    for (int i = 0; i < 7; i++)
+    {   
+        if (max = num[i]){max_count++;}
+        if (min = num[i]){min_count++;}
+        if (i == 7) { bool out = true; }
+    }
+    if (bool out = true)
+    {
+        cout << "Максимальное значение: " << max << endl;
+        cout << "Минимальное значение: " << min << endl;
+        cout << "Повторения максимального числа: " << max_count << endl;
+        cout << "Повторения минимального числа: " << min_count << endl;
+    }
+}
